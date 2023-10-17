73d8ec2af9d0f4fae77410542132647ec95ceb63
Tobias Felipe Kiefer <tobias.f.kiefer@gmail.com>
2023-10-17 20:34:40 -0300

    aplicado solver

--- alocacaoAntenas.py
+++ alocacaoAntenas.py
@@ -10,7 +10,6 @@ ny = None  # Coordenada y de pontos de demanda
 mx = None  # Coordenada x de locais candidatos
 my = None  # Coordenada y de locais candidatos
 
-
 def read_instance(instance):
     global A, B, C, D, mx, my, nx, ny
     nx = []
@@ -35,37 +34,44 @@ def read_instance(instance):
             mx.append(int(line.split(' ')[1]))
             my.append(int(line.split(' ')[2]))
 
+def distance(i, j):
+    # Função para calcular a distância entre os pontos i e j -> ** 0.5 é usado para calcular a raiz quadrada
+    return ((mx[i] - nx[j]) ** 2 + (my[i] - ny[j]) ** 2) ** 0.5
 
 def solve():
+
     # Criação do modelo
     model = ConcreteModel()
 
     # Variáveis de decisão
-    model.a = Var(range(A), domain=Boolean)
-    model.b = Var(range(B), domain=Boolean)
+    model.a = Var(range(A), domain = Binary)
+    model.b = Var(range(B), domain = Binary)
 
     # Função objetivo
+    model.obj = Objective(expr=sum([C * model.a[i] for i in range(A)])+sum([min([distance(i, j) for j in range(B)]) * model.b[i] for i in range(B)]),sense=minimize)
 
     # Restrições
+    # for i in range(B): model.con1 = Constraint(expr=sum([model.a[j] for j in range(A)]) >= 1)
+    # for i in range(B): model.con2 = Constraint(expr= model.b[i] in [0, 1])
+    # for j in range(A): model.con3 = Constraint(expr= model.a[j] in [0, 1])
+
+    model.peloMenosUmaAntena = ConstraintList()
+    for i in range(B):
+        model.peloMenosUmaAntena.add(sum(model.a[j] for j in range(A)) >= 1)
+
+    model.varBinaria = ConstraintList()
+    for i in range(B):
+        model.varBinaria.add(0 <= model.b[i] <= 1)
+    for j in range(A):
+        model.varBinaria.add(0 <= model.a[j] <= 1)
 
     # Solução
     opt = SolverFactory('glpk')
-    # opt.solve(model).write()
     opt.solve(model)
-    # for i in range(n):
-    #    print(model.x[i]())
     return model.obj.expr()
 
 
-for instance in glob('./instanciaPequena1.txt'):
+for instance in glob('./instancias/instanciaPequena1.txt'):
     read_instance(instance)
-    print(A)
-    print(B)
-    print(C)
-    print(D)
-    print(nx)
-    print(ny)
-    print(mx)
-    print(my)
-    # print(instance[instance.rindex('/') + 1:] + ': ', end = '')
-    # print(solve())
+    print(instance[instance.rindex('/') + 1:] + ': ', end = '')
+    print(solve())
