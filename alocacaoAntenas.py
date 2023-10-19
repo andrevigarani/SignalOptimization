cc070e306a7cb57bf6444b1056378a1d3a85cdc2
tobiasfkk <78160146+tobiasfkk@users.noreply.github.com>
2023-10-18 22:49:41 -0300

    Update alocacaoAntenas.py
     tentatica de corrigir a Função objetivo mas tem q testar

--- alocacaoAntenas.py
+++ alocacaoAntenas.py
@@ -45,9 +45,16 @@ def solve():
     model.b = Var(range(B), domain=Binary)
 
     # Função objetivo
+    # model.obj = Objective(
+    #     expr=sum([C * model.a[j] for j in range(A)]) * -1
+    #          + sum([min([distance(i, j) for j in range(A)]) * model.b[i] for i in range(B)]),
+    #     sense=maximize
+    # )
+
+    # tentatica de corrigir a Função objetivo mas tem q testar
     model.obj = Objective(
-        expr=sum([C * model.a[j] for j in range(A)]) * -1
-             + sum([min([distance(i, j) for j in range(A)]) * model.b[i] for i in range(B)]),
+        expr=sum([C * model.a[j] for j in range(A)])
+             + sum([min([distance(i, j) for j in range(A)]) for i in range(B)]),
         sense=maximize
     )
 
